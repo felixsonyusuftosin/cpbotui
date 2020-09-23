85c196d757c0fe64fbe0855e476005655fdb4869
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-09-23 12:47:42 +0100

    add conditions to manage data flow

--- src/newComponent/components/Table.jsx
+++ src/newComponent/components/Table.jsx
@@ -51,7 +51,7 @@ export default () => {
       await fetchUserList()
       return
     }
-    if (value.length > 2 && !loading) {
+    if ( !loading) {
       setFilter(value)
       setFilterLoading(true)
       const token = await firebase.auth().currentUser.getIdToken(true)
